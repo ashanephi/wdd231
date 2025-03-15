69a364aa6dcbf655e351ff1806b6c58dc3046641
ashanephi <ash24013@byui.edu>
2025-03-15 20:59:47 +0100

    Updated code for the Commerce Site

---
+++ chamber/scripts/script.js
@@ -0,0 +1,43 @@
+const dateModifier = document.querySelector("#lastModified");
+dateModifier.textContent = document.lastModified;
+
+fetch("http://192.168.65.176:5500/chamber/members.json")
+.then(response => response.json())
+.then(data => displayMembers(data));
+
+function displayMembers(members) {
+    const directory = document.querySelector(".container");
+
+    directory.innerHTML = "";
+
+    members.forEach(member => {
+        let card = document.createElement("section");
+        card.classList.add("member-card");
+        card.innerHTML = `
+        <h2>${member.name}</h2>
+        <p>${member.tagline}</p>
+        <hr>
+        <div class="contact">
+            <div class="business-profile-image">
+                <img src="${member.image}" alt="${member.name}" width="100">
+            </div>
+            <div class="information">
+                <p><strong>ADDRESS: </strong>${member.address}</p>
+                <p><strong>PHONE: </strong>${member.phone}</p>
+                <p><strong>URL: </strong><a href="${member.website}" target="_blank">${member.website}</a></p>          
+                <p><strong>MEMBERSHIP: </strong>Platinum</p>                
+            </div>
+        </div>
+        `;
+
+        directory.appendChild(card);
+    })
+
+}
+
+function toggleMenu(element) {
+    const navMenu = document.querySelector("nav ul");
+
+    navMenu.classList.toggle("active");
+    element.classList.toggle("active");
+}
--- scripts/prophets.js
+++ scripts/prophets.js
@@ -1,3 +1,6 @@
+const dateModifier = document.querySelector("#lastModified");
+dateModifier.textContent = document.lastModified;
+
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 
 const cards = document.querySelector('#cards');
